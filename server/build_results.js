7399f1157bae171060d6b4ba51780fc54b987396
Hackfront <hackfront@protonmail.ch>
2019-03-30 15:38:02 +0100

    Convert multiline strings to arrays in build_results.js

--- server/build_results.js
+++ server/build_results.js
@@ -164,7 +164,19 @@ db.each('SELECT id, username, code, timestamp FROM userdata ORDER BY timestamp D
 
 		var lastsavedate = new Date(userTimestamps[currusername]);
 		var lastsavetime = getLastSaveTime(lastsavedate);
-		var contestTableHtml = '<div id="results"><h1 align="center">Verifica della gara del '+lastsavedate.getDate()+'/'+(lastsavedate.getMonth()+1)+'/'+lastsavedate.getFullYear()+'</h1><p>Per provare usare il <a href="#simulator">simulatore</a> pre-caricato con i problemi della squadra \'<i>'+sanitizer.escape(String(currusername))+'</i>\'.</p><h2>Sommario ('+points+' punt'+(points === 1 ? 'o' : 'i')+', ultimo salvataggio '+lastsavetime+')</h2><table border="1"><tr><th>Problem</th><th>Points</th><th>Result</th><th>Count</th></tr>';
+		var contestTableHtml = [
+			'<div id="results">',
+			'  <h1 align="center">Verifica della gara del ' + lastsavedate.getDate() + '/' + (lastsavedate.getMonth() + 1) + '/' + lastsavedate.getFullYear() + '</h1>',
+			'  <p>Per provare usare il <a href="#simulator">simulatore</a> pre-caricato con i problemi della squadra \'<i>' + sanitizer.escape(String(currusername)) + '</i>\'.</p>',
+			'  <h2>Sommario (' + points + ' punt' + (points === 1 ? 'o' : 'i') + ', ultimo salvataggio ' + lastsavetime + ')</h2>',
+			'  <table border="1">',
+			'    <tr>',
+			'      <th>Problem</th>',
+			'      <th>Points</th>',
+			'      <th>Result</th>',
+			'      <th>Count</th>',
+			'    </tr>'
+		].join('\n');
 		Object.keys(contestProblems).forEach(function(currproblemid){
 			var currproblem = contestProblems[currproblemid];
 			contestTableHtml += '<tr><td><a';
@@ -189,12 +201,32 @@ db.each('SELECT id, username, code, timestamp FROM userdata ORDER BY timestamp D
 				return;
 			}
 			var curruserproblem = currproblem.userdata[currusername];
-			contestTableHtml += '<h2><a name="'+sanitizer.escape(String(currproblemid))+'">'+sanitizer.escape(String(currproblem.name))+'</a> ('+currproblem.points+' punt'+(currproblem.points === 1 ? 'o' : 'i')+')</h2><table border="1"><tr><th>Outcome</th><th>Input</th><th>Output</th><th>Atteso</th><th>Passi</th><th>Risultato</th></tr>';
+			contestTableHtml += [
+				'<h2><a name="' + sanitizer.escape(String(currproblemid)) + '">' + sanitizer.escape(String(currproblem.name)) + '</a> (' + currproblem.points + ' punt' + (currproblem.points === 1 ? 'o' : 'i') + ')</h2>',
+				'<table border="1">',
+				'  <tr>',
+				'    <th>Outcome</th>',
+				'    <th>Input</th>',
+				'    <th>Output</th>',
+				'    <th>Atteso</th>',
+				'    <th>Passi</th>',
+				'    <th>Risultato</th>',
+				'  </tr>'
+			].join('\n');
 			if (curruserproblem.error) {
 				contestTableHtml += '<tr><td colspan="6" style="color:#f00">'+sanitizer.escape(String((curruserproblem.error.errorType === 'syntax' ? String(currlang.SYNTAX_ERROR_LABEL).replace('%d', curruserproblem.error.errorLine + 1) : currlang.UNKNOWN_ERROR_LABEL) + ' ' + currlang[curruserproblem.error.errorMessage]))+'</td></tr>';
 			} else {
 				curruserproblem.testcases.forEach(function(currtestcase){
-					contestTableHtml += '<tr><td>'+sanitizer.escape(String(currtestcase.outcome))+'</td><td class="codecell">'+sanitizer.escape(String(currtestcase.initialtape))+'</td><td class="codecell">'+sanitizer.escape(String(currtestcase.finaltape))+'</td><td class="codecell">'+sanitizer.escape(String(currtestcase.expectedtape))+'</td><td>'+currtestcase.steps+'</td><td class="'+(currtestcase.success ? 'green' : 'red')+'">'+(currtestcase.success ? 'OK' : 'FAIL')+'</td></tr>';
+					contestTableHtml += [
+						'<tr>',
+						'  <td>' + sanitizer.escape(String(currtestcase.outcome)) + '</td>',
+						'  <td class="codecell">'+sanitizer.escape(String(currtestcase.initialtape)) + '</td>',
+						'  <td class="codecell">'+sanitizer.escape(String(currtestcase.finaltape)) + '</td>',
+						'  <td class="codecell">'+sanitizer.escape(String(currtestcase.expectedtape)) + '</td>',
+						'  <td>' + currtestcase.steps + '</td>',
+						'  <td class="' + (currtestcase.success ? 'green' : 'red') + '">' + (currtestcase.success ? 'OK' : 'FAIL') + '</td>',
+						'</tr>'
+					].join('\n');
 				});
 			}
 			contestTableHtml += '</table>';
@@ -211,7 +243,32 @@ db.each('SELECT id, username, code, timestamp FROM userdata ORDER BY timestamp D
 			preloadedProblems.push({name: currproblem.name, code: currcode});
 			fs.writeFileSync(path.join(resultsDirName, currusername, 'progs', currproblemid + '.t'), currcode);
 		});
-		fs.writeFileSync(path.join(resultsDirName, currusername, 'index.html'), ('<!doctype html>\n<!-- saved from url=(0014)about:internet -->\n<!--\n' + licenseText + '--><html class="notranslate"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><title>Turing Machine Competition Results</title>' + htmlheadHtml + '<style>' + uglifycss(String(cssStyle)) + '</style>' + iecsshacksHtml + '</head><body class="loadmode displayresults">' + contestTableHtml + turingMachineHtml + '<script>' + uglifyjs.minify('(function(){' + turingMachineJS + i18nJS + handleHTMLPageJS + 'var preloadedProblems = '+JSON.stringify(preloadedProblems)+';' + handlePreloadedProblemsJS + '})();', {fromString: true}).code + '</script></body></html>').replace(new RegExp('(?:\\r\\n|\\n|\\r)', 'g'), '\r\n')); // just another IE 6 fix
+		fs.writeFileSync(path.join(resultsDirName, currusername, 'index.html'), [
+			'<!doctype html>',
+			'<!-- saved from url=(0014)about:internet -->',
+			'<!--',
+			licenseText,
+			'-->',
+			'<html class="notranslate">',
+			'<head>',
+			'  <meta charset="utf-8">',
+			'  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">',
+			'  <title>Turing Machine Competition Results</title>',
+			htmlheadHtml,
+			'  <style>',
+			uglifycss(String(cssStyle)),
+			'  </style>',
+			iecsshacksHtml,
+			'</head>',
+			'<body class="loadmode displayresults">',
+			contestTableHtml,
+			turingMachineHtml,
+			'  <script>',
+			uglifyjs.minify('(function(){' + turingMachineJS + i18nJS + handleHTMLPageJS + 'var preloadedProblems = ' + JSON.stringify(preloadedProblems) + ';' + handlePreloadedProblemsJS + '})();', {fromString: true}).code,
+			'  </script>',
+			'</body>',
+			'</html>'
+		].join('\n').replace(new RegExp('(?:\\r\\n|\\n|\\r)', 'g'), '\r\n')); // just another IE 6 fix
 		fs.writeFileSync(path.join(resultsDirName, currusername, 'jstmsimulator.gif'), jstmsimulatorGif);
 
 		userPoints[currusername] = points;
@@ -222,7 +279,17 @@ db.each('SELECT id, username, code, timestamp FROM userdata ORDER BY timestamp D
 	});
 	var lastsavedate = new Date(lastTimestamp);
 	var lastsavetime = getLastSaveTime(lastsavedate);
-	var contestTableHtml = '<div id="results"><h1 align="center">Classifica della gara del '+lastsavedate.getDate()+'/'+(lastsavedate.getMonth()+1)+'/'+lastsavedate.getFullYear()+'</h1><h2>Ultimo salvataggio globale: '+lastsavetime+'</h2><table border="1"><tr><th>Posizione</th><th>Nome utente</th><th>Punteggio</th><th>Ultimo salvataggio</th>';
+	var contestTableHtml = [
+		'<div id="results">',
+		'<h1 align="center">Classifica della gara del ' + lastsavedate.getDate() + '/' + (lastsavedate.getMonth() + 1) + '/' + lastsavedate.getFullYear() + '</h1>',
+		'<h2>Ultimo salvataggio globale: ' + lastsavetime + '</h2>',
+		'<table border="1">',
+		'<tr>',
+		'<th>Posizione</th>',
+		'<th>Nome utente</th>',
+		'<th>Punteggio</th>',
+		'<th>Ultimo salvataggio</th>'
+	].join('\n');
 	var contestCSV = '"Nome utente";"Posizione";"Punteggio";"Ultimo salvataggio"\n';
 	Object.keys(contestProblems).forEach(function(currproblemid){
 		var currproblem = contestProblems[currproblemid];
@@ -264,7 +331,28 @@ db.each('SELECT id, username, code, timestamp FROM userdata ORDER BY timestamp D
 	});
 	contestTableHtml += '</table></div>';
 
-	fs.writeFileSync(path.join(resultsDirName, 'index.html'), ('<!doctype html>\n<!-- saved from url=(0014)about:internet -->\n<!--\n' + licenseText + '--><html class="notranslate"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><title>Turing Machine Competition Results</title>' + htmlheadHtml + '<style>' + uglifycss(String(cssStyle)) + '</style>' + iecsshacksHtml + '</head><body class="loadmode displayresults">' + contestTableHtml + '</body></html>').replace(new RegExp('(?:\\r\\n|\\n|\\r)', 'g'), '\r\n')); // just another IE 6 fix
+	fs.writeFileSync(path.join(resultsDirName, 'index.html'), [
+		'<!doctype html>',
+		'<!-- saved from url=(0014)about:internet -->',
+		'<!--',
+		licenseText,
+		'-->',
+		'<html class="notranslate">',
+		'<head>',
+		'  <meta charset="utf-8">',
+		'  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">',
+		'  <title>Turing Machine Competition Results</title>',
+		htmlheadHtml,
+		'  <style>',
+		uglifycss(String(cssStyle)),
+		'  </style>',
+		iecsshacksHtml,
+		'</head>',
+		'<body class="loadmode displayresults">' ,
+		 contestTableHtml,
+		'</body>',
+		'</html>'
+	].join('\n').replace(new RegExp('(?:\\r\\n|\\n|\\r)', 'g'), '\r\n')); // just another IE 6 fix
 	fs.writeFileSync(path.join(resultsDirName, 'index.csv'), contestCSV);
 
 	console.log('Results written into ' + resultsDirName);
